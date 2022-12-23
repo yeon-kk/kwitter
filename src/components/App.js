034ae6b73d38fac2a50ff933d2d85a110f3cabe4
yeonkyoung <keep.ykk@gmail.com>
2022-12-23 13:18:52 +0900

    feat: add OAuth and Router

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import AppRouter from "components/Router";
+import { useState, useEffect } from "react";
+import { authService } from "fbase";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsloggedIn(user);
+        setUserObj(user);
+      } else {
+        setIsloggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "initializing..."
+      )}
+      <footer>&copy; {new Date().getFullYear()} Kwitter</footer>
+    </>
+  );
+}
+
+export default App;
