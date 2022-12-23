f7115a23843034ff7840956b97af413b1b5543dd
yeonkyoung <keep.ykk@gmail.com>
2022-12-23 13:28:06 +0900

    feat: Profile Component, logout and navigate page

---
+++ src/routes/Profile.js
@@ -0,0 +1,19 @@
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const navigate = useNavigate();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
+export default Profile;
